35b55e5cd92dcfb3937dadcc2749e24d35dd142b
bpetterborg <ben.petterborg@gmail.com>
2020-11-01 11:54:48 -0500

    Added stuff for flask

--- control.py
+++ control.py
@@ -4,15 +4,13 @@
 # TODO:
 #  - write GUI frontend (probably in Flask)
 #  - add support for: 
-#       - reading temperatures (DHT11 or something)
-#       - reading humidity
-#       - reading water barrel level
-#  - add matplotlib graphs
+#       - reading water storage level
+#  - add matplotlib graphs in flask
 
 # importing stuff
 from gpiozero import LED  # controls rpi gpio
 from time import sleep
-import Flask # web frontend
+from flask import Flask, render_template # web frontend
 import board
 import adafruit_dht # read DHT11 data
 
@@ -20,10 +18,12 @@ import adafruit_dht # read DHT11 data
 relay0 = LED(26)
 #relay1 = # figure out what pins these go to
 #relay2 = 
+app = Flask(__name__)
 
 tempSensor = adafruit_dht.DHT11(board.D17)
 tempSensorSleepTime = 2.0 # don't read temp sensor too frequently
 
+# get temp and humidity
 while True:
     try:
         global temp
@@ -38,3 +38,17 @@ while True:
         print(error.args[0]) # these sensors make errors often
 
     time.sleep(tempSensorSleepTime)
+
+# print data to page
+@app.route('/')
+def home():
+    
+    # print data
+    return temp
+    return humidity
+    # add matplotlib graphs
+
+
+if __name__ == '__main__':
+    app.run()
+    
