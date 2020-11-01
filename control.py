65491ee6ee1811ded80732abd07c877d0be7c6c7
Benjamin Petterborg <68480079+bpetterborg@users.noreply.github.com>
2020-10-31 23:19:21 -0400

    Create control.py

---
+++ control.py
@@ -0,0 +1,7 @@
+# Controls irrigation system for garden, using
+# a Raspberry Pi.
+
+# TODO:
+#  - add some code
+#  - write GUI frontend (probably in Flask)
+#  - add support for reading temperatures
