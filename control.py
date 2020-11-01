8e02ecb5f0f0e0763edfdc43cce0760731b40df9
bpetterborg <ben.petterborg@gmail.com>
2020-11-01 08:39:52 -0500

    Added stuff to read DHT11 data

--- control.py
+++ control.py
@@ -1,7 +1,6 @@
 # Controls irrigation system for garden, using
 # a Raspberry Pi.
 
-
 # TODO:
 #  - write GUI frontend (probably in Flask)
 #  - add support for: 
@@ -14,13 +13,28 @@
 from gpiozero import LED  # controls rpi gpio
 from time import sleep
 import Flask # web frontend
-import Adafruit_DHT # read DHT11 data
+import board
+import adafruit_dht # read DHT11 data
 
 # variables
 relay0 = LED(26)
 #relay1 = # figure out what pins these go to
 #relay2 = 
 
-tempSensor = Adafruit_DHT.DHT11 # set temp sensor type
-tempSensePin = 4 # set temp sensor pin
-humidity, temp = Adafruit_DHT.read_retry(tempSensor, tempSensePin)
+tempSensor = adafruit_dht.DHT11(board.D17)
+tempSensorSleepTime = 2.0 # don't read temp sensor too frequently
+
+while True:
+    try:
+        global temp
+        global humidity
+        temp = tempSensor.temperature
+        humidity = tempSensor.humidity
+        
+        print('temp =' + temp) # change these to print to page
+        print('humidity =' + humidity)
+    
+    except RuntimeError as error:
+        print(error.args[0]) # these sensors make errors often
+
+    time.sleep(tempSensorSleepTime)
