4134eaa5fa5c21b3bd73379335ed5a16ecef0b38
bpetterborg <ben.petterborg@gmail.com>
2020-10-31 23:43:32 -0400

    Added stuff for reading temp, humidity

--- control.py
+++ control.py
@@ -1,20 +1,26 @@
 # Controls irrigation system for garden, using
 # a Raspberry Pi.
 
+
 # TODO:
-#  - add some code
 #  - write GUI frontend (probably in Flask)
 #  - add support for: 
-#       - reading temperatures
+#       - reading temperatures (DHT11 or something)
 #       - reading humidity
 #       - reading water barrel level
+#  - add matplotlib graphs
 
 # importing stuff
-from gpiozero import LED as relay # controls rpi gpio
+from gpiozero import LED  # controls rpi gpio
 from time import sleep
 import Flask # web frontend
+import Adafruit_DHT # read DHT11 data
 
 # variables
-relay0 = relay(26)
+relay0 = LED(26)
 #relay1 = # figure out what pins these go to
 #relay2 = 
+
+tempSensor = Adafruit_DHT.DHT11 # set temp sensor type
+tempSensePin = 4 # set temp sensor pin
+humidity, temp = Adafruit_DHT.read_retry(tempSensor, tempSensePin)
