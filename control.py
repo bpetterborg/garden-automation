8e7f8e12f72e03e2628df5eddd2ce8c422bfce10
bpetterborg <ben.petterborg@gmail.com>
2020-10-31 23:30:30 -0400

    Importing stuff

--- control.py
+++ control.py
@@ -4,4 +4,17 @@
 # TODO:
 #  - add some code
 #  - write GUI frontend (probably in Flask)
-#  - add support for reading temperatures
+#  - add support for: 
+#       - reading temperatures
+#       - reading humidity
+#       - reading water barrel level
+
+# importing stuff
+from gpiozero import LED as relay # controls rpi gpio
+from time import sleep
+import Flask # web frontend
+
+# variables
+relay0 = relay(26)
+#relay1 = # figure out what pins these go to
+#relay2 = 
