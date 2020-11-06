bfd80d79892f43bdd5c4a6f6b24e4c8e2febb550
bpetterborg <ben.petterborg@gmail.com>
2020-11-06 11:51:25 -0500

    Minor changes, still WIP

--- control.py
+++ control.py
@@ -6,46 +6,58 @@
 #  - add features: 
 #       - reading water storage level
 #       - reading soil moisture levels
-#       - manual mode
-#       - remotely setting of relayXTimeOn
-#  - add matplotlib graphs, display in flask
+#       - manual watering mode
+#       - remotely setting relayXTimeOn
+#  - add matplotlib graphs, display in flask with pyplot
 #  - test everything, on RPi
 
-# importing stuff
-from gpiozero import LED, DigitalInputDevice  # controls rpi gpio
-from time import sleep
+# importing stuff for displaying data
 from flask import Flask, render_template # web frontend
-import board
+import matplotlib.pyplot as plot # pyplot graphing  (still need to install)
+import numpy # math stuff
+
+# importing stuff for sensors
+from gpiozero import LED, DigitalInputDevice  # controls rpi gpio
+import time
+import board 
 import adafruit_dht # read DHT11 data
 
 # variables
-relay0 = LED(26)
+# relay pins
+relay0 = LED(26) # these are gpio, not board pins
 relay1 = LED(20)
 relay2 = LED(21)
+
+# time to open relays for (s)
 relay0TimeOn = 120 # solenoid (set correct values)
 relay1TimeOn = 120 # pump
 relay2TimeOn = 120 # unused
 
 # yl69/fc28 sensor
 soilMoistureSensor0 = DigitalInputDevice(4) # change 4 to whatever pin is in use
-# will have at least 3 of these, maybe 6(?)
+# will have at least 3 of these, maybe 6(?). figure out which pins
 
 app = Flask(__name__)
 
 # dht11 sensor
-tempSensor = adafruit_dht.DHT11(board.D17)
-tempSensorSleepTime = 2.0 # don't read temp sensor too frequently
-temp = tempSensor.temperature
-humidity = tempSensor.humidity
+tempSensorSleepTime = 2.0 # don't read temp sensor too frequently, it will freak out
+
+tempSensor0 = adafruit_dht.DHT11(board.D17) # change these to not use board pins
+#tempSensor1 = adafruit_dht.DHT11(board D17)
+
+temp0 = tempSensor0.temperature
+humidity0 = tempSensor0.humidity
 
 # relay control (3ch)
+# solenoid
 def activateRelay0():
-    print('activating relay0')
+    print('activating relay0') # print fun stuff to the TTY
     relay0.on()
     sleep(relay0TimeOn)
     print('deactivating relay0')
     relay0.off()
 
+# pump
 def activateRelay1():
     print('activating relay1')
     relay1.on()
@@ -53,6 +65,7 @@ def activateRelay1():
     print('deactivating relay1')
     relay1.off()
 
+# unused
 def activateRelay2():
     print('activating relay2')
     relay0.on()
@@ -68,17 +81,25 @@ while True:
     else:
         # run function to activate relay
         print('Level Low')
+        activateRelay0()
 
-# get temp and humidity
+# get temp and humidity from tempSensor0
 while True:
     try:
-        print('temp =' + temp) # change these to print to page
-        print('humidity =' + humidity)
+        print('temp =' + temp0) # change these to print to page
+        print('humidity =' + humidity0)
     
     except RuntimeError as error:
         print(error.args[0]) # these sensors make errors often
 
-    time.sleep(tempSensorSleepTime)
+    # getting data from sensors too often results in a lot of errors and bad readings
+    sleep(tempSensorSleepTime)
+
+# pyplot things
+# use a for loop to fetch past 48 hours of data
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+plot.show()
+
 
 # print data to page
 @app.route('/')
@@ -86,9 +107,11 @@ def home():
     
     # print data
     return temp
-    return humidity
+    # add soil moisture sensor state
     # add matplotlib graphs
+
+# print graphs to page (pyplot)
+#def graphs():
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
-    
